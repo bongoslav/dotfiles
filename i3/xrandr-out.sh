5873e388ad3c048c15e9e064fefc0bc5c4a7b1c7
bongoslav <beborisov99@gmail.com>
2023-05-08 11:06:00 +0300

    added polybar and updated folder structure

---
+++ i3/xrandr-out.sh
@@ -0,0 +1,1 @@
+xrandr --output LVDS-1 --off --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off
---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
+
+# launch on multiple monitors
+#if type "xrandr"; then
+#  for m in $(polybar --list-monitors | cut -d":" -f1); do
+#    MONITOR=$m polybar --reload example &
+#  done
+#else
+#  polybar --reload example &
+#fi
