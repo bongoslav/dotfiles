6f0d22e0d98850e1c22b69cb061a1fecb5a6396f
bongoslav <beborisov99@gmail.com>
2023-08-23 12:58:17 +0300

    added fish, omf, nvim to repo

---
+++ nvim/lua/bongoslav/plugins/lsp/test.ts
@@ -0,0 +1,6 @@
+const a = 5;
+
+
+for const b of [1,2,3] {
+  console.log(b);
+}
---
+++ nvim/lua/bongoslav/plugins/lualine.lua
@@ -0,0 +1,11 @@
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+-- get lualine nightfly theme
+local lualine_nightfly = require("lualine.themes.nightfly")
+
+-- configure lualine with modified theme
+lualine.setup()
+
